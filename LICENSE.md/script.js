d891c184de7f4e59258a09ed409f263313bc2c70
NukeReactor <jaydenwyatt100@gmail.com>
2018-06-08 15:56:18 -0400

    Update script.js

--- LICENSE.md/script.js
+++ LICENSE.md/script.js
@@ -1,17 +1,17 @@
 $(function() {
 	$(".hidden").slideToggle();
-})
+});
 
 $(function() {
-	$(".menuToggle").click(function() {
+	$("#menuToggle").click(function() {
 		$(".navbar").slideToggle(500);
 		$(".hidden").slideToggle(500);
 	});
-})
+});
 
 $(function() {
 	$(".showMenu").click(function() {
 		$(".navbar").slideToggle(500);
 		$(".hidden").slideToggle(500);
 	})
-})
+});
