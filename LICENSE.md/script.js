448bb4335f8d8c7969b2e9e4899bffcee2288b01
NukeReactor <jaydenwyatt100@gmail.com>
2018-06-08 13:12:16 -0400

    Create script.js

---
+++ LICENSE.md/script.js
@@ -0,0 +1,17 @@
+$(function() {
+	$(".hidden").slideToggle();
+})
+
+$(function() {
+	$(".menuToggle").click(function() {
+		$(".navbar").slideToggle(500);
+		$(".hidden").slideToggle(500);
+	});
+})
+
+$(function() {
+	$(".showMenu").click(function() {
+		$(".navbar").slideToggle(500);
+		$(".hidden").slideToggle(500);
+	})
+})
